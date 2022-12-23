9147ed8501ae1f1e26f02822de2537ce9b2da5d5
DineshMv <dineshmv38@gmail.com>
2022-12-23 07:48:39 -0600

    Unit Testing

---
+++ 7. Errors and Exceptions Handling/4. Unit Test/cap.py
@@ -0,0 +1,12 @@
+def cap_text(text):
+    """
+    Input a string
+    output is a capitalised text
+    :return:
+    """
+    # return text.capitalize()
+
+    # capitalize() only capitalize the first letter of the word
+    # title() capitalize every first letter of the words
+
+    return text.title()
